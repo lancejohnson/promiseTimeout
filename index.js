9fa972625352c9299445576127340fb4edff27b6
Lance Johnson <lancejohnson728@gmail.com>
2019-08-13 09:38:41 -0400

    Retries if condition not met

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const fs = require("fs");
 const ip = require("ip");
 
 function timeOutTest() {
-    // Will resolve after 5000ms
     let id;
     let promiseA = new Promise((resolve, reject) => {
     id = setTimeout(() => {
-      resolve('Promise A Wins!')
-  }, 2000)
+      resolve(Math.floor(Math.random() * Math.floor(2)));
+  }, 20000)
     })
 
-    // Will resolve after 200ms
     let promiseB = new Promise((resolve, reject) => {
     id1 = setTimeout(() => {
         var x = 5 + 7;
         resolve(x);
-  }, 200)
+  }, 2000)
     })
 
     // Let's race our promises
@@ -34,6 +32,9 @@ function timeOutTest() {
 
 async function runTimeout() {
     var message = await timeOutTest();
+    if (message == 1) {
+        runTimeout();
+    }
     console.log(message);
 }
 
